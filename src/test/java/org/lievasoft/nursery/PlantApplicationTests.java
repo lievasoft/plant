eb465b4bb82b692277d3293f84d0ecfb7447a3a9
JosMaria <josmariaguilar@gmail.com>
2024-09-17 14:58:14 -0400

    initial commit

---
+++ src/test/java/org/lievasoft/nursery/PlantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lievasoft.nursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
